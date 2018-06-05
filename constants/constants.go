434aef5ece6f2afb63751516cafef2746675fb6e
Glen D Sanford <9len@users.noreply.github.com>
2018-06-05 02:40:31 +0000

    prepare for release of OSS version 0.17.1 (#5485)
    fbshipit-source-id: 1c0b648

--- constants/constants.go
+++ constants/constants.go
@@ -23,4 +23,4 @@ const DefaultClusterLabelName = "tbn_cluster"
 
 // TbnPublicVersion is the current version of all Turbine Labs open-source
 // software and artifacts.
-const TbnPublicVersion = "0.17.0"
+const TbnPublicVersion = "0.17.1"
