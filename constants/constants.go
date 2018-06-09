4ef5dbdb69300f4eaaf99fb4ea7ba56e0c7353bb
Felix Yan <felixonmars@archlinux.org>
2018-06-09 17:29:46 +0800

    Fix a typo in constants.go

--- constants/constants.go
+++ constants/constants.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package constants contains constants shared accross plugin packages.
+// Package constants contains constants shared across plugin packages.
 package constants
 
 // DefaultClusterLabelName is the default name for a service discovery label
